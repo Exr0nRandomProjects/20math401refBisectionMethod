45d7b7f636154cfc763ad006d7cc4e41d2d71ba3
Exr0n <spotyie@gmail.com>
2020-10-15 10:04:57 -0700

    support custom y value

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 poly = input('please input the polynomial: ')
 
-l = int(input('left? '))
-r = int(input('right? '))
+y = float(input('value? '))
+l = float(input('left? '))
+r = float(input('right? '))
 
 if l > r:
     l, r = r, l
 
 while r - l > 1e-5:
     mid = (l + r)/2
-    if (eval(poly.replace('x', str(mid))) > 0) == (l > 0):
+    if (eval(poly.replace('x', str(mid))) > y) == (l > y):
         l = mid
     else:
         r = mid
